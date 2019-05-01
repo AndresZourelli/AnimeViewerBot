836437a4be7bc06462bc7f20bb4f5d679161bc38
Andres <azourelli@gmail.com>
2019-04-30 20:28:24 -0500

    created connections

--- code/Bot.py
+++ code/Bot.py
@@ -1,3 +1,22 @@
+from accounts import token
+import discord
+import json
+data2 = json.load(open('data.json', 'r'))
 
-from accounts import token 
-print('hello')
+
+class MyClient(discord.Client):
+    async def on_ready(self):
+        print('Logged on as', self.user)
+
+    async def on_message(self, message):
+        if message.author == self.user:
+            return
+
+        if message.content.startswith('.'):
+            await message.channel.send('pong')
+            await message.channel.send(data2['anime'][:5])
+
+
+######Run Code#####
+client = MyClient()
+client.run(token)
--- code/scrapping.py
+++ code/scrapping.py
@@ -1,5 +1,9 @@
 import requests
 from bs4 import BeautifulSoup
+import json
+
+data = {}
+data['anime'] = []
 
 webpage = 'https://myanimelist.net/anime/season/schedule'
 
@@ -13,7 +17,7 @@ weekdays = [
     'js-seasonal-anime-list-key-thursday', 'js-seasonal-anime-list-key-friday',
     'js-seasonal-anime-list-key-saturday', 'js-seasonal-anime-list-key-sunday'
 ]
-
+i = 1
 for day in weekdays:
     name_box = soup.findAll('div', attrs={'class': day})
     for value in name_box:
@@ -23,4 +27,23 @@ for day in weekdays:
             producer = name.find('span', attrs={'class': 'producer'})
             episodes = name.find('div', attrs={'class': 'eps'})
             airTime = name.find('span', attrs={'class': 'remain-time'})
+            rating = name.find('span', attrs={'class': 'score'})
+            data['anime'].append({
+                'id':
+                i,
+                'title':
+                title_tag.text.replace('\n', '').strip(),
+                'producer':
+                producer.text.replace('\n', '').strip(),
+                'episodes':
+                episodes.text.replace('\n', '').strip(),
+                'airTime':
+                airTime.text.replace('\n', '').strip(),
+                'rating':
+                rating.text.replace('\n', '').strip()
+            })
             print(title_tag.text, airTime.text)
+            i += 1
+
+with open('data.json', 'w') as outfile:
+    json.dump(data, outfile)
