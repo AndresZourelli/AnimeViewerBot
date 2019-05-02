ce38d0f71b2a970675ab32197a731209cb3b780c
Andres <azourelli@gmail.com>
2019-05-02 17:26:37 -0500

    updated files to new format

--- code/Bot.py
+++ code/Bot.py
@@ -2,29 +2,39 @@ from accounts import token
 import discord
 import json
 import datetime
+from discord.ext import commands
 data2 = json.load(open('data.json', 'r'))
 userData = json.load(open('userData.json', 'r'))
 
+client = commands.Bot(command_prefix='.')
 
-class MyClient(discord.Client):
-    async def on_ready(self):
-        print('Logged on as', self.user)
 
-    async def on_message(self, message):
-        if message.author == self.user:
-            return
+@client.event
+async def on_ready():
+    print('Logged on as', client.user)
 
-        if message.content.startswith('.'):
-            await message.channel.send('pong')
-            for anime in data2['anime'][:5]:
-                embed = discord.Embed(title=anime['title'],
-                                      description=anime['description'],
-                                      colour=discord.Colour.blue())
-                embed.set_image(url=anime['image'])
 
-                await message.channel.send(embed=embed)
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('.'):
+        await message.channel.send('pong')
+        for anime in data2['anime'][:5]:
+            embed = discord.Embed(title=anime['title'],
+                                  description=anime['description'],
+                                  colour=discord.Colour.blue())
+            embed.set_image(url=anime['image'])
+
+            await message.channel.send(embed=embed)
+
+
+@client.event
+async def on_reaction_add(reaction, user):
+    print(reaction, user, reaction.message)
 
 
 ######Run Code#####
-client = MyClient()
+
 client.run(token)
